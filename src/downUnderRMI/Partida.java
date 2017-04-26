34b4e49e31663d67e79888727c96a86b5953eacd
unknown <12205139@w3043494.portoalegre.pucrsnet.br>
2017-04-25 22:12:04 -0300

    valida jogador

--- src/downUnderRMI/Partida.java
+++ src/downUnderRMI/Partida.java
@@ -2,13 +2,33 @@ package downUnderRMI;
 
 public class Partida {
 	
-	private int jogador1, jogador2;
+	private Jogador jogador1, jogador2;
 	private Tabuleiro tabuleiro;
 	
-	public Partida(int jogador1, int jogador2) {
+	public Partida(Jogador jogador1) {
 		this.jogador1 = jogador1;
-		this.jogador2 = jogador2;
 		
 		tabuleiro = new Tabuleiro();
+	}
+	
+	public boolean addJogador(Jogador jogador2) {
+		if (this.jogador2 != null)
+			return false;
+		
+		this.jogador2 = jogador2;
+		return true;
+	}
+	
+	public boolean existeJogador(String nomeJogador) {
+		return jogador1.getNome().toLowerCase() == nomeJogador.toLowerCase() ||
+				jogador2.getNome().toLowerCase() == nomeJogador.toLowerCase();
+	}
+	
+	public boolean existeJogador(int id) {
+		return jogador1.getId() == id || jogador2.getId() == id;
+	}
+	
+	public boolean partidaFechada() {
+		return jogador1 != null && jogador2 != null;
 	}
 }
